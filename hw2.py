8173943952e444ce536f6e0d508ce2e2aa46b523
Alagar-samy <110388626+Alagar-samy@users.noreply.github.com>
2022-08-09 19:52:43 +0530

    Add files via upload

---
+++ hw2.py
@@ -0,0 +1,21 @@
+budget=int(input("enter the budget"))
+petrol=3
+budget-=petrol
+city=input("enter the city")
+if(city=='madurai'):
+    onion=int(input("enter the price"))
+    tomato=int(input("enter the price"))
+    print("onions",budget//onion)
+    print("tomato",budget//tomato)
+elif(city=='chennai'):
+    onion=int(input("enter the price"))
+    tomato=int(input("enter the price"))
+    print("onions",budget//onion)
+    print("tomato",budget//tomato)
+else:
+    onion=int(input("enter the price"))
+    tomato=int(input("enter the price"))
+    print("onions",budget//onion)
+    print("tomato",budget//tomato)
+    
+    
